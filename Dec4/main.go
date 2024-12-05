32d313abf855be8ae9ae0eaecb42ceecb6d18235
Neil2020 <neil.thomas@mccorkell.com.au>
2024-12-05 13:23:31 +1100

    Updated for Day 4

--- Dec4/main.go
+++ Dec4/main.go
@@ -6,59 +6,114 @@ import (
 	"strings"
 )
 
-func checkIfElements(Arr string) bool {
-	for z := 0; z < len(Arr); z++ {
-		if z+4 > len(Arr) {
+func checkIfElementsPart1(input string) bool {
+	for z := 0; z < len(input); z++ {
+		//Check min length
+		if z+4 > len(input) {
 			break
 		}
-		if Arr[z:z+4] == "XMAS" || Arr[z:z+4] == "SAMX" {
+		//Check to validate + return bool
+		if input[z:z+4] == "XMAS" || input[z:z+4] == "SAMX" {
 			return true
 		}
 	}
 	return false
 }
-
-func main() {
-	fileData, err := os.ReadFile("./input/sample.txt")
+func Part1() {
+	//Read Files
+	fileData, err := os.ReadFile("./input/input.txt")
 	if err != nil {
 		panic(err)
 	}
 	rows := strings.Split(string(fileData), "\r\n")
 	var count int = 0
+	var ArrOfStrings []string
 	for i := 0; i < len(rows); i++ {
-		var horiArr string = string(rows[i])
-		if len(rows)+i >= 4 {
-			//Create Virtical String
-			for v := 0; v < len(rows[i]); v++ {
-				if string(rows[i][v]) == "X" || string(rows[i][v]) == "S" {
+		//Create Virtical String
+		for v := 0; v < len(rows[i]); v++ {
+			//Check to see if you need to create Arrays based on on the current value
+			if string(rows[i][v]) == "X" || string(rows[i][v]) == "S" {
+				//Add to Array Horizontal Line || no further check needed
+				if len(rows[i][v:]) >= 4 {
+					ArrOfStrings = append(ArrOfStrings, string(rows[i][v:v+4]))
+				}
+				//Virtical and Diagonal arrays need to have min 3 rows below
+				if len(rows)-i > 3 {
 					var virtArr string = ""
-					var diagArr string = ""
-					for vA := 0; vA == 4; vA++ {
+					var diagRArr string = ""
+					var diagLArr string = ""
+					//Loop to create the virtical + diagonal strings
+					for vA := 0; vA < 4; vA++ {
 						if vA == 0 {
+							//initial string for 'X' OR 'S'
 							virtArr = string(rows[i+vA][v])
-							diagArr = string(rows[i+vA][v])
+							diagRArr = string(rows[i+vA][v])
+							diagLArr = string(rows[i+vA][v])
 						} else {
+							//Append to virt and diagonal strings
 							virtArr = virtArr + string(rows[i+vA][v])
-							diagArr = diagArr + string(rows[i+vA][v+1]) // Does not Work
+							if len(rows[i+vA][v:]) > 3 {
+								diagRArr = diagRArr + string(rows[i+vA][v+vA:][0])
+							}
+							if len(rows[i+vA][:v]) >= 3 {
+								diagLArr = diagLArr + string(rows[i+vA][:v][len(rows[i+vA][:v-vA])])
+							}
 						}
 					}
-					//Check for Virtical String
-					if checkIfElements(string(virtArr)) {
-						count++
-					}
-					//Create Diagonal String
-					if checkIfElements(string(diagArr)) {
+					//Add to Array for Virtical String
+					ArrOfStrings = append(ArrOfStrings, string(virtArr))
+					//Add to Array Diagonal Right String
+					ArrOfStrings = append(ArrOfStrings, string(diagRArr))
+					ArrOfStrings = append(ArrOfStrings, string(diagLArr))
+				}
+			}
+		}
+	}
+	for _, value := range ArrOfStrings {
+		//Checking Strings
+		if checkIfElementsPart1(value) {
+			count++
+		}
+	}
+	fmt.Println("Count of XMAS: ", count)
+}
+func checkIfElementsPart2(inputOne string, inputTwo string) bool {
+	//Check if the inputs provided creates a X-MAS one way or the other
+	if (inputOne == "MAS" || inputOne == "SAM") && (inputTwo == "MAS" || inputTwo == "SAM") {
+		return true
+	}
+	return false
+}
+func Part2() {
+	//Read file
+	fileData, err := os.ReadFile("./input/input.txt")
+	if err != nil {
+		panic(err)
+	}
+	//Get  rows of data
+	rows := strings.Split(string(fileData), "\r\n")
+	var count int = 0
+	//Loop Through Rows
+	for i := 0; i <= (len(rows) - 1); i++ {
+		if (len(rows) - 1) <= i {
+			break
+		}
+		//Skip first and last Row as cannot make an X-MAS with no rows above or below
+		if i != 0 && i <= (len(rows)-1) {
+			//Loop through value of Rows
+			for z := 0; z < len(rows[i]); z++ {
+				//if Value is "A" check the correct values the the row above and below and Check Elements to see if they make and X-MAS
+				if string(rows[i][z]) == "A" && len(rows[i][:z]) >= 1 && len(rows[i][z:]) > 1 {
+					if checkIfElementsPart2(string(string(rows[i-1][z-1])+string(rows[i][z])+string(rows[i+1][z+1])), string(string(rows[i+1][z-1])+string(rows[i][z])+string(rows[i-1][z+1]))) {
 						count++
 					}
 				}
 			}
-
-		}
-
-		//Check For horizontal String
-		if checkIfElements(horiArr) {
-			count++
 		}
 	}
-	fmt.Println(count)
+	fmt.Println("Results: ", count)
+}
+func main() {
+	Part1()
+	Part2()
 }
